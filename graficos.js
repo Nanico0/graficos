1ff27c4f45f9ce9a2c76bcbab419f1336bb08f12
Nanico0 <168193542+Nanico0@users.noreply.github.com>
2024-10-16 08:16:14 -0300

    Add files via upload

--- graficos.js
+++ graficos.js
@@ -28,6 +28,36 @@ const graficoPizza = new Chart(ctxPiza, {
     }
 });
 
+// Gráficos de Pizza
+const ctxPiza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPiza, {
+    type: 'pie',
+    data: {
+        labels: ['Diariamente', 'Algumas vezes por semana', 'raramente', 'mensalmente'],
+        dataets: [{
+        label: 'Com que frequência você ouve música?',
+            data: [78,17, 6],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Com que frequência você ouve música?'
+            }
+        }
+    }
+});
+
 //Gráfico de Barras
 const ctxBarras = document.getElementById('graficoBarra').getContext('2d');
 const graficosBarra = new Chart(ctxBarra, {
@@ -35,7 +65,7 @@ const graficosBarra = new Chart(ctxBarra, {
     data: {
         labels: ['Diariamente', 'Algumas vezes por semana', 'raramente', 'mensalmente'],
         datasets: [{
-            label: 'Com que frequência você ouve música?',
+            label: 'Qual é a sua banda ou artita favorito?',
             data: [7800, 560, 1700, 0],
             backgroundColor: [
                 '#F05454', '#30475E' , '#DDDDDD' , '#222831'
@@ -58,6 +88,126 @@ const graficosBarra = new Chart(ctxBarra, {
         scales: {
             y: {
                 beginAtZero: true
+            }
+        }
+    }
+});
+
+// Gráficos de Pizza
+const ctxPiza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPiza, {
+    type: 'pie',
+    data: {
+        labels: ['Sim', 'Não'],
+        dataets: [{
+        label: 'Você toca algum intrumento musical?',
+            data: [17,83],
+            backgroundColor: [
+                '#F05454', '#30475E'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Você toca algum instrumento musical?'
+            }
+        }
+    }
+});
+
+// Gráficos de Pizza
+const ctxPiza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPiza, {
+    type: 'pie',
+    data: {
+        labels: ['Sim', 'Não', 'Raramente'],
+        dataets: [{
+        label: 'Você costuma ir a shows ou festivais ?',
+            data: [5,6; 55,6; 38,9],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Você costuma ir a shows ou festivais?'
+            }
+        }
+    }
+});
+
+// Gráficos de Pizza
+const ctxPiza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPiza, {
+    type: 'pie',
+    data: {
+        labels: ['Sozinho', 'Em grupo'],
+        dataets: [{
+        label: 'Você prefere ouvir músicas sozinho ou em grupo?',
+            data: [89; 11],
+            backgroundColor: [
+                '#F05454', '#30475E'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Você prefere ouvir músicas sozinho ou em grupo?'
+            }
+        }
+    }
+});
+
+// Gráficos de Pizza
+const ctxPiza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPiza, {
+    type: 'pie',
+    data: {
+        labels: ['Sim', 'não'],
+        dataets: [{
+        label: 'Você ja produziu alguma música?',
+            data: [5, 95],
+            backgroundColor: [
+                '#F05454', '#30475E'
+            ],
+            borderColor: '#DDDDDD',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responive: true,
+        plugins: {
+            legend: {
+                position: 'bottom',
+            },
+            title: {
+                display: true,
+                text: 'Você ja produziu alguma música?'
             }
         }
     }
